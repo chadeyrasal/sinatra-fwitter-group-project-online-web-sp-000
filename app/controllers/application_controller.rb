08475d77adf2c3c8e98fd658e27b7748cba1f32b
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-20 14:57:54 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
+  enable :sessions
+  use Rack::Flash
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
