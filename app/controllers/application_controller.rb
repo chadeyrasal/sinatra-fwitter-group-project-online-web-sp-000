5056cdd4d95573954fd8c18567a48e203314bd3a
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-20 15:10:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     params.each do |label, user_input|
-      if input.empty?
+      if user_input.empty?
         flash[:signup_error] = "Please enter a value for #{label}"
         redirect "/signup"
       end
