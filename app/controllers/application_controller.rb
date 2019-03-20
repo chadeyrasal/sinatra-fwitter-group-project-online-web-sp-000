582f40be06bacc1f76646e1285594caf49330bf8
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2019-03-20 15:08:58 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -54,7 +54,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/tweets" do
-    if !!session[:user_id]
+    if logged_in?
       @tweets = Tweet.all
       erb :"tweets/index"
     else
